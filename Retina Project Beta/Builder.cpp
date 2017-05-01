2e63adc3ff89d12ad2531840cda353c0841ea2d3
tareyza <tareyza@yahoo.com>
2017-05-01 16:22:56 -0400

    Added stuff for integration function.

--- Retina Project Beta/Builder.cpp
+++ Retina Project Beta/Builder.cpp
@@ -4,9 +4,12 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
+#include <array>
+#include <random>
 #include <vector>
 #include <cmath>
 #include <ctime>
+#include <iterator>
 #include "Neuron.h"
 #include "Photoreceptor.h"
 #include "Cone.h"
@@ -22,6 +25,23 @@ const double probabilityGREEN=0.32;
 //probabilityBlue= 0.02;
 
 Quadtree<Photoreceptor> retina;
+
+const int numCone = 10000;//number of cones to be generated
+const int numRds = 10000;//number of rods to be generated
+
+//eccentricities for data points for photoreceptors
+std::array<double, 12> coneIntervals{ 0.12219, 7.9600897, 17.49904, 26.76169, 35.5145, 54.50688, 72.42695, 90.7516, 108.60589, 126.01612, 144.24867, 162.93843 };
+std::array<double, 16> rodIntervals{ 9.147886, 17.805035, 26.424519, 35.26334, 44.18192, 52.72386, 62.198536, 70.36384, 79.412025, 88.8867, 98.371346, 107.429504, 116.45775, 125.505936, 134.09773, 143.59235 };
+
+//densities in 1000s of photoreceptors/mm^2 for each interval
+std::array<double, 12> coneDensity{ 201.819, 150.17027, 111.00869, 88.49225, 69.91545, 50.293564, 41.721466, 35.550728, 30.941757, 26.3093, 25.686943, 24.295444 };
+std::array<double, 16> rodDensity{ 64.36445, 109.88923, 129.6439, 135.57826, 136.48705, 132.98442, 126.99707, 119.08304, 111.82526, 105.837906, 99.22235, 91.33638, 85.335, 78.07722, 71.43361, 64.189865 };
+
+std::piecewise_linear_distribution<double>
+coneDistribution(coneIntervals.begin(), coneIntervals.end(), coneDensity.begin());
+
+std::piecewise_linear_distribution<double>
+rodDistribution(rodIntervals.begin(), rodIntervals.end(), rodDensity.begin());
 
 Cone* buildCone() {
 	// Method creates rods and cones based on polar coordinates 
@@ -48,6 +68,9 @@ Cone* buildCone() {
 }
 
 double getDistance(bool isCone) {
+
+
+
 	return ((double)rand() / (RAND_MAX)) * 180;
 }
 
@@ -69,7 +92,7 @@ Rod* buildRod() {
 
 int main()
 {
-	
+		
 }
 /*
 int previousImplementation() {
